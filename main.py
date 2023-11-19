a35e7c47ef3ce104aa4774dc49bb0b981f70f99e
NathanP23 <nathanp20@icloud.com>
2023-11-19 20:13:31 +0200

    Changed Epoch to 5000, added print for final predictions in binary form, added documentation.

--- main.py
+++ main.py
@@ -25,7 +25,38 @@ if __name__ == "__main__":
     nn = NeuralNetwork(input_size=2, hidden_size=4, output_size=1)
 
     """
-    Invokes the train() method of the neural network instance nn. It trains the neural network using the provided inputs and targets data over 1000 epochs with a learning rate of 0.1. 
+    Invokes the train() method of the neural network instance nn. It trains the neural network using the provided inputs and targets data over 5000 epochs with a learning rate of 0.1. 
     This initiates the training process for the neural network, which includes forward and backward passes to update the weights and biases based on the provided data.
     """
-    nn.train(inputs, targets, epochs=1000, learning_rate=0.1)
+    nn.train(inputs, targets, epochs=5000, learning_rate=0.1)
+
+
+    """
+    After training, predict outputs for test inputs
+    """
+    test_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+    predictions = nn.feedforward(test_inputs)
+    print("Test Inputs:")
+    print(test_inputs)
+
+
+    """
+    The decimal values outputted by the neural network represent the network's confidence or estimation of the output being 1 for each input. 
+    """
+    print("Predicted Outputs:")
+    print(predictions)
+
+    """
+    After predicting outputs for test inputs. We can adjust this threshold as needed.
+    """
+    threshold = 0.5
+
+    """
+    Now converting the confidence values to binary values using the threshold.
+    """
+    binary_predictions = np.round(predictions).astype(int)
+    binary_predictions[binary_predictions <= threshold] = 0
+    binary_predictions[binary_predictions > threshold] = 1
+
+    print("Binary Predicted Outputs: ")
+    print(binary_predictions)
